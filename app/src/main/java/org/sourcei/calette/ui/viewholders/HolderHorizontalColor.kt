43a6aa050116d6939d932020e7b494f9230296cf
Saksham Khurana <dawnimpulse@gmail.com>
2019-08-16 14:31:15 +0530

    New : Colors

---
+++ app/src/main/java/org/sourcei/calette/ui/viewholders/HolderHorizontalColor.kt
@@ -0,0 +1,51 @@
+/**
+ * ISC License
+ *
+ * Copyright 2018-2019, Saksham (DawnImpulse)
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+ * OR PERFORMANCE OF THIS SOFTWARE.
+ **/
+package org.sourcei.calette.ui.viewholders
+
+import android.view.View
+import androidx.core.graphics.toColorInt
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.inflator_horizontal_color.view.*
+import org.sourcei.calette.ui.pojo.PojoColor
+
+/**
+ * @info -
+ *
+ * @author - Saksham
+ * @note Last Branch Update - master
+ *
+ * @note Created on 2019-08-16 by Saksham
+ * @note Updates :
+ */
+class HolderHorizontalColor(view: View) : RecyclerView.ViewHolder(view) {
+
+    var layout = view.horizontalColorLayout
+    var code = view.horizontalColorCode
+    var name = view.horizontalColorName
+    var bookmark = view.horizontalBookmark
+
+    /**
+     * set details
+     */
+    fun setDetails(color:PojoColor){
+        layout.setBackgroundColor(color.color.toColorInt())
+        code.text = color.color
+        name.text = color.name
+
+        bookmark.setOnClickListener {
+
+        }
+    }
+}
---
+++ app/src/main/java/org/sourcei/calette/utils/reusables/Arrays.kt
@@ -0,0 +1,342 @@
+/**
+ * ISC License
+ *
+ * Copyright 2018-2019, Saksham (DawnImpulse)
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+ * OR PERFORMANCE OF THIS SOFTWARE.
+ **/
+package org.sourcei.calette.utils.reusables
+
+import org.sourcei.calette.ui.pojo.PojoColor
+
+/**
+ * @info -
+ *
+ * @author - Saksham
+ * @note Last Branch Update - master
+ *
+ * @note Created on 2019-08-16 by Saksham
+ * @note Updates :
+ */
+object Arrays {
+
+    val red = listOf(
+            PojoColor("#ffebee", "50"),
+            PojoColor("#ffcdd2", "100"),
+            PojoColor("#ef9a9a", "200"),
+            PojoColor("#e57373", "300"),
+            PojoColor("#ef5350", "400"),
+            PojoColor("#f44336", "500"),
+            PojoColor("#f44336", "600"),
+            PojoColor("#d32f2f", "700"),
+            PojoColor("#c62828", "800"),
+            PojoColor("#b71c1c", "900"),
+            PojoColor("#ff8a80", "a100"),
+            PojoColor("#ff5252", "a200"),
+            PojoColor("#ff1744", "a400"),
+            PojoColor("#d50000", "a700")
+    )
+
+    val pink = listOf(
+            PojoColor("#fce4ec", "50"),
+            PojoColor("#f8bbd0", "100"),
+            PojoColor("#f48fb1", "200"),
+            PojoColor("#f06292", "300"),
+            PojoColor("#ec407a", "400"),
+            PojoColor("#e91e63", "500"),
+            PojoColor("#d81b60", "600"),
+            PojoColor("#c2185b", "700"),
+            PojoColor("#ad1457", "800"),
+            PojoColor("#880e4f", "900"),
+            PojoColor("#ff80ab", "a100"),
+            PojoColor("#ff4081", "a200"),
+            PojoColor("#f50057", "a400"),
+            PojoColor("#c51162", "a700")
+    )
+
+    val purple = listOf(
+            PojoColor("#f3e5f5", "50"),
+            PojoColor("#e1bee7", "100"),
+            PojoColor("#ce93d8", "200"),
+            PojoColor("#ba68c8", "300"),
+            PojoColor("#ab47bc", "400"),
+            PojoColor("#9c27b0", "500"),
+            PojoColor("#8e24aa", "600"),
+            PojoColor("#7b1fa2", "700"),
+            PojoColor("#6a1b9a", "800"),
+            PojoColor("#4a148c", "900"),
+            PojoColor("#ea80fc", "a100"),
+            PojoColor("#e040fb", "a200"),
+            PojoColor("#d500f9", "a400"),
+            PojoColor("#aa00ff", "a700")
+    )
+
+    val deeppurple = listOf(
+            PojoColor("#ede7f6", "50"),
+            PojoColor("#d1c4e9", "100"),
+            PojoColor("#b39ddb", "200"),
+            PojoColor("#9575cd", "300"),
+            PojoColor("#7e57c2", "400"),
+            PojoColor("#673ab7", "500"),
+            PojoColor("#5e35b1", "600"),
+            PojoColor("#512da8", "700"),
+            PojoColor("#4527a0", "800"),
+            PojoColor("#311b92", "900"),
+            PojoColor("#b388ff", "a100"),
+            PojoColor("#7c4dff", "a200"),
+            PojoColor("#651fff", "a400"),
+            PojoColor("#6200ea", "a700")
+    )
+
+    val indigo = listOf(
+            PojoColor("#e8eaf6", "50"),
+            PojoColor("#c5cae9", "100"),
+            PojoColor("#9fa8da", "200"),
+            PojoColor("#7986cb", "300"),
+            PojoColor("#5c6bc0", "400"),
+            PojoColor("#3f51b5", "500"),
+            PojoColor("#3949ab", "600"),
+            PojoColor("#303f9f", "700"),
+            PojoColor("#283593", "800"),
+            PojoColor("#1a237e", "900"),
+            PojoColor("#8c9eff", "a100"),
+            PojoColor("#536dfe", "a200"),
+            PojoColor("#3d5afe", "a400"),
+            PojoColor("#304ffe", "a700")
+    )
+
+    val blue = listOf(
+            PojoColor("#e3f2fd", "50"),
+            PojoColor("#bbdefb", "100"),
+            PojoColor("#90caf9", "200"),
+            PojoColor("#64b5f6", "300"),
+            PojoColor("#42a5f5", "400"),
+            PojoColor("#2196f3", "500"),
+            PojoColor("#1e88e5", "600"),
+            PojoColor("#1976d2", "700"),
+            PojoColor("#1565c0", "800"),
+            PojoColor("#0d47a1", "900"),
+            PojoColor("#82b1ff", "a100"),
+            PojoColor("#448aff", "a200"),
+            PojoColor("#2979ff", "a400"),
+            PojoColor("#2962ff", "a700")
+    )
+
+    val lightblue = listOf(
+            PojoColor("#e1f5fe", "50"),
+            PojoColor("#b3e5fc", "100"),
+            PojoColor("#81d4fa", "200"),
+            PojoColor("#4fc3f7", "300"),
+            PojoColor("#29b6f6", "400"),
+            PojoColor("#03a9f4", "500"),
+            PojoColor("#039be5", "600"),
+            PojoColor("#0288d1", "700"),
+            PojoColor("#0277bd", "800"),
+            PojoColor("#01579b", "900"),
+            PojoColor("#80d8ff", "a100"),
+            PojoColor("#40c4ff", "a200"),
+            PojoColor("#00b0ff", "a400"),
+            PojoColor("#0091ea", "a700")
+    )
+
+    val cyan = listOf(
+            PojoColor("#e0f7fa", "50"),
+            PojoColor("#b2ebf2", "100"),
+            PojoColor("#80deea", "200"),
+            PojoColor("#4dd0e1", "300"),
+            PojoColor("#26c6da", "400"),
+            PojoColor("#00bcd4", "500"),
+            PojoColor("#00acc1", "600"),
+            PojoColor("#0097a7", "700"),
+            PojoColor("#00838f", "800"),
+            PojoColor("#006064", "900"),
+            PojoColor("#84ffff", "a100"),
+            PojoColor("#18ffff", "a200"),
+            PojoColor("#00e5ff", "a400"),
+            PojoColor("#00b8d4", "a700")
+    )
+
+    val teal = listOf(
+            PojoColor("#e0f2f1", "50"),
+            PojoColor("#b2dfdb", "100"),
+            PojoColor("#80cbc4", "200"),
+            PojoColor("#4db6ac", "300"),
+            PojoColor("#26a69a", "400"),
+            PojoColor("#009688", "500"),
+            PojoColor("#00897b", "600"),
+            PojoColor("#00796b", "700"),
+            PojoColor("#00695c", "800"),
+            PojoColor("#004d40", "900"),
+            PojoColor("#a7ffeb", "a100"),
+            PojoColor("#64ffda", "a200"),
+            PojoColor("#1de9b6", "a400"),
+            PojoColor("#00bfa5", "a700")
+    )
+
+    val green = listOf(
+            PojoColor("#e8f5e9", "50"),
+            PojoColor("#c8e6c9", "100"),
+            PojoColor("#a5d6a7", "200"),
+            PojoColor("#81c784", "300"),
+            PojoColor("#66bb6a", "400"),
+            PojoColor("#4caf50", "500"),
+            PojoColor("#43a047", "600"),
+            PojoColor("#388e3c", "700"),
+            PojoColor("#2e7d32", "800"),
+            PojoColor("#1b5e20", "900"),
+            PojoColor("#b9f6ca", "a100"),
+            PojoColor("#69f0ae", "a200"),
+            PojoColor("#00e676", "a400"),
+            PojoColor("#00c853", "a700")
+    )
+
+    val lightgreen = listOf(
+            PojoColor("#f1f8e9", "50"),
+            PojoColor("#dcedc8", "100"),
+            PojoColor("#c5e1a5", "200"),
+            PojoColor("#aed581", "300"),
+            PojoColor("#9ccc65", "400"),
+            PojoColor("#8bc34a", "500"),
+            PojoColor("#7cb342", "600"),
+            PojoColor("#689f38", "700"),
+            PojoColor("#558b2f", "800"),
+            PojoColor("#33691e", "900"),
+            PojoColor("#ccff90", "a100"),
+            PojoColor("#b2ff59", "a200"),
+            PojoColor("#76ff03", "a400"),
+            PojoColor("#64dd17", "a700")
+    )
+
+    val lime = listOf(
+            PojoColor("#f9fbe7", "50"),
+            PojoColor("#f0f4c3", "100"),
+            PojoColor("#e6ee9c", "200"),
+            PojoColor("#dce775", "300"),
+            PojoColor("#d4e157", "400"),
+            PojoColor("#cddc39", "500"),
+            PojoColor("#c0ca33", "600"),
+            PojoColor("#afb42b", "700"),
+            PojoColor("#9e9d24", "800"),
+            PojoColor("#827717", "900"),
+            PojoColor("#f4ff81", "a100"),
+            PojoColor("#eeff41", "a200"),
+            PojoColor("#c6ff00", "a400"),
+            PojoColor("#aeea00", "a700")
+    )
+
+    val yellow = listOf(
+            PojoColor("#fffde7", "50"),
+            PojoColor("#fff9c4", "100"),
+            PojoColor("#fff59d", "200"),
+            PojoColor("#fff176", "300"),
+            PojoColor("#ffee58", "400"),
+            PojoColor("#ffeb3b", "500"),
+            PojoColor("#fdd835", "600"),
+            PojoColor("#fbc02d", "700"),
+            PojoColor("#f9a825", "800"),
+            PojoColor("#f57f17", "900"),
+            PojoColor("#ffff8d", "a100"),
+            PojoColor("#ffff00", "a200"),
+            PojoColor("#ffea00", "a400"),
+            PojoColor("#ffd600", "a700")
+    )
+
+    val amber = listOf(
+            PojoColor("#fff8e1", "50"),
+            PojoColor("#ffecb3", "100"),
+            PojoColor("#ffe082", "200"),
+            PojoColor("#ffd54f", "300"),
+            PojoColor("#ffca28", "400"),
+            PojoColor("#ffc107", "500"),
+            PojoColor("#ffb300", "600"),
+            PojoColor("#ffa000", "700"),
+            PojoColor("#ff8f00", "800"),
+            PojoColor("#ff6f00", "900"),
+            PojoColor("#ffe57f", "a100"),
+            PojoColor("#ffd740", "a200"),
+            PojoColor("#ffc400", "a400"),
+            PojoColor("#ffab00", "a700")
+    )
+
+    val orange = listOf(
+            PojoColor("#fff3e0", "50"),
+            PojoColor("#ffe0b2", "100"),
+            PojoColor("#ffcc80", "200"),
+            PojoColor("#ffb74d", "300"),
+            PojoColor("#ffa726", "400"),
+            PojoColor("#ff9800", "500"),
+            PojoColor("#fb8c00", "600"),
+            PojoColor("#f57c00", "700"),
+            PojoColor("#ef6c00", "800"),
+            PojoColor("#e65100", "900"),
+            PojoColor("#ffd180", "a100"),
+            PojoColor("#ffab40", "a200"),
+            PojoColor("#ff9100", "a400"),
+            PojoColor("#ff6d00", "a700")
+    )
+
+    val deeporange = listOf(
+            PojoColor("#fbe9e7", "50"),
+            PojoColor("#ffccbc", "100"),
+            PojoColor("#ffab91", "200"),
+            PojoColor("#ff8a65", "300"),
+            PojoColor("#ff7043", "400"),
+            PojoColor("#ff5722", "500"),
+            PojoColor("#f4511e", "600"),
+            PojoColor("#e64a19", "700"),
+            PojoColor("#d84315", "800"),
+            PojoColor("#bf360c", "900"),
+            PojoColor("#ff9e80", "a100"),
+            PojoColor("#ff6e40", "a200"),
+            PojoColor("#ff3d00", "a400"),
+            PojoColor("#dd2c00", "a700")
+    )
+
+    val brown = listOf(
+            PojoColor("#efebe9", "50"),
+            PojoColor("#d7ccc8", "100"),
+            PojoColor("#bcaaa4", "200"),
+            PojoColor("#a1887f", "300"),
+            PojoColor("#8d6e63", "400"),
+            PojoColor("#795548", "500"),
+            PojoColor("#6d4c41", "600"),
+            PojoColor("#5d4037", "700"),
+            PojoColor("#4e342e", "800"),
+            PojoColor("#3e2723", "900")
+    )
+
+    val grey = listOf(
+            PojoColor("#efebe9", "50"),
+            PojoColor("#d7ccc8", "100"),
+            PojoColor("#bcaaa4", "200"),
+            PojoColor("#a1887f", "300"),
+            PojoColor("#8d6e63", "400"),
+            PojoColor("#795548", "500"),
+            PojoColor("#6d4c41", "600"),
+            PojoColor("#5d4037", "700"),
+            PojoColor("#4e342e", "800"),
+            PojoColor("#3e2723", "900")
+    )
+
+    val bluegrey = listOf(
+            PojoColor("#eceff1", "50"),
+            PojoColor("#cfd8dc", "100"),
+            PojoColor("#b0bec5", "200"),
+            PojoColor("#90a4ae", "300"),
+            PojoColor("#78909c", "400"),
+            PojoColor("#607d8b", "500"),
+            PojoColor("#546e7a", "600"),
+            PojoColor("#455a64", "700"),
+            PojoColor("#37474f", "800"),
+            PojoColor("#263238", "900")
+    )
+
+    var materialColorsList = listOf(red, pink, purple, deeppurple, indigo, blue, lightblue, cyan, teal, green, lightgreen, lime, yellow, amber, orange, deeporange, brown, grey, bluegrey)
+}
